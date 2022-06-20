ba28e044d592c2904cbbc0d8e736e7fe5663c880
SatohaCC <42375182+SatohaCC@users.noreply.github.com>
2022-06-20 21:02:01 +0900

    img→Image

--- components/Layout.js
+++ components/Layout.js
@@ -4,40 +4,43 @@ import utilStyles from "../styles/utils.module.css";
 const name = "Satoha Code";
 export const siteTitle = "Next.js blog";
 import Link from "next/link";
+import Image from "next/image";
 
 function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <heade className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.png"
-              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
-            />
-            <h1 className={utilStyles.headingXl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <img
-              src="/images/profile.png"
-              className={`${utilStyles.borderCircle} `}
-            />
-            <h1 className={utilStyles.headingXl}>{name}</h1>
-          </>
-        )}
-      </heade>
-      <main>{children}</main>
-      {!home && (
-        <div>
-          <Link href="/">ホームへ戻る</Link>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<heade className={styles.header}>
+				{home ? (
+					<>
+						<Image
+							src="/images/profile.png"
+							className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+							alt=""
+						/>
+						<h1 className={utilStyles.headingXl}>{name}</h1>
+					</>
+				) : (
+					<>
+						<Image
+							src="/images/profile.png"
+							className={`${utilStyles.borderCircle} `}
+							alt=""
+						/>
+						<h1 className={utilStyles.headingXl}>{name}</h1>
+					</>
+				)}
+			</heade>
+			<main>{children}</main>
+			{!home && (
+				<div>
+					<Link href="/">ホームへ戻る</Link>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Layout;
